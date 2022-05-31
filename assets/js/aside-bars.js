f0a404d5a5f4d7ba5936a56036bd8351637c22bd
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2022-05-31 00:08:10 -0300

    finalizando primeira parte do painel do admin

--- assets/js/aside-bars.js
+++ assets/js/aside-bars.js
@@ -1,19 +1,37 @@
-const btnAbrir = document.querySelector('.menu-icone')
+const btnOpenSide = document.querySelector('.btn__menu-aside')
 const menu = document.querySelectorAll('.aside__menu-admin')
-const menuBi = document.querySelector('.menu-icone .bi')
+const changeIcon = document.querySelector('.btn__menu-aside .bi')
+const changeLogo = document.querySelector('.dashboard__logo-img')
+const dashboardSearch = document.querySelector('.dashboard__research')
+const dashboardLink = document.querySelectorAll('.dashboard__link')
 
+for (let i = 0; i < menu.length; i++) {
+    btnOpenSide.addEventListener('click', function () {
+        menu[i].classList.toggle('disabled-display')
 
-for (let i = 0; menu.length > i; i++) {
-    btnAbrir.addEventListener('click', function () {
-        menu[i].classList.toggle('aside__menu-admin--hide')
+        function changeAttrClass(first, second) {
+            changeIcon.getAttribute('class', first )
+            changeIcon.setAttribute('class', second)
+        }
+        
+        function changeAttrSrc(first, second) {
+            changeLogo.getAttribute('src', first )
+            changeLogo.setAttribute('src', second)
+        }
+        
+        if (changeIcon.getAttribute('class') == 'bi bi-chevron-bar-left') {
+            changeAttrClass('bi bi-chevron-bar-left', 'bi bi-chevron-bar-right')
+            changeAttrSrc('/assets/images/logo-ponto-animes-admin-01.svg', '/assets/images/logo-ponto-animes-admin-02.svg')
 
-        if (menuBi.getAttribute('class') == 'bi bi-chevron-bar-left') {
-            menuBi.getAttribute('class', 'bi bi-chevron-bar-left')
-            menuBi.setAttribute('class', 'bi bi-chevron-bar-right')
+            dashboardSearch.classList.add('disabled-display')
+            dashboardLink[i].classList.toggle('justify__center')
 
-        } else if (menuBi.getAttribute('class') == 'bi bi-chevron-bar-right') {
-            menuBi.getAttribute('class', 'bi bi-chevron-bar-right')
-            menuBi.setAttribute('class', 'bi bi-chevron-bar-left')
+        } else if (changeIcon.getAttribute('class') == 'bi bi-chevron-bar-right') {
+            changeAttrClass('bi bi-chevron-bar-right', 'bi bi-chevron-bar-left')
+            changeAttrSrc('/assets/images/logo-ponto-animes-admin-02.svg', '/assets/images/logo-ponto-animes-admin-01.svg')
+
+            dashboardSearch.classList.remove('disabled-display')
+            dashboardLink[i].classList.toggle('justify__center')
         }
 
     })
