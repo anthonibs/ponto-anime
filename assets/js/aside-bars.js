344cffa152dcc37be10a2d9c4a5bd48597801455
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2022-06-01 06:35:13 -0300

    separação dos arquivos css
    Separação dos arquivos css da página painel de controles e aplicação de botões

--- assets/js/aside-bars.js
+++ assets/js/aside-bars.js
@@ -1,24 +1,26 @@
 const btnOpenSide = document.querySelector('.btn__menu-aside')
 const menu = document.querySelectorAll('.aside__menu-admin')
-const changeIcon = document.querySelector('.btn__menu-aside .bi')
+const changeIcon = document.querySelector('.bi')
 const changeLogo = document.querySelector('.dashboard__logo-img')
 const dashboardSearch = document.querySelector('.dashboard__research')
 const dashboardLink = document.querySelectorAll('.dashboard__link')
+const btnNotificacao = document.querySelector('.dashboard__notification .bi')
+
 
 for (let i = 0; i < menu.length; i++) {
     btnOpenSide.addEventListener('click', function () {
         menu[i].classList.toggle('disabled-display')
 
-        function changeAttrClass(first, second) {
-            changeIcon.getAttribute('class', first )
-            changeIcon.setAttribute('class', second)
-        }
-        
         function changeAttrSrc(first, second) {
             changeLogo.getAttribute('src', first )
             changeLogo.setAttribute('src', second)
         }
         
+        function changeAttrClass(first, second) {
+            changeIcon.getAttribute('class', first )
+            changeIcon.setAttribute('class', second)
+        }
+                
         if (changeIcon.getAttribute('class') == 'bi bi-chevron-bar-left') {
             changeAttrClass('bi bi-chevron-bar-left', 'bi bi-chevron-bar-right')
             changeAttrSrc('/assets/images/logo-ponto-animes-admin-01.svg', '/assets/images/logo-ponto-animes-admin-02.svg')
@@ -36,3 +38,28 @@ for (let i = 0; i < menu.length; i++) {
 
     })
 }
+
+
+
+// Ativar e desativar notificação
+btnNotificacao.addEventListener('dblclick', function() {
+
+    const notificacao = document.querySelector('.number-notification')
+
+    if(btnNotificacao.getAttribute('class') == 'bi bi-bell-fill') {
+        btnNotificacao.getAttribute('class', 'bi bi-bell-fill')
+        btnNotificacao.setAttribute('class', 'bi bi-bell-slash-fill')
+
+        notificacao.style.visibility = 'hidden'
+
+    } else if(btnNotificacao.getAttribute('class') == 'bi bi-bell-slash-fill') {
+        btnNotificacao.getAttribute('class', 'bi bi-bell-slash-fill')
+        btnNotificacao.setAttribute('class', 'bi bi-bell-fill')
+
+        notificacao.style.visibility = 'visible'
+
+
+    }
+    
+
+})
